632291337cb3864b7282f29102a7279b9e6a1651
Kishore4f2 <satyakishore273@gmail.com>
2025-08-29 15:59:13 +0530

    input

--- Pal.java
+++ Pal.java
@@ -1,8 +1,11 @@
+import java.util.*;
 public class Pal {
     public static void main(String[] args) {
         //program to print the numbers which are not having digit '5'
         //wap to check given palindrome or not.
-        int num = 12321;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
         int rev = 0;
         int temp = num;
         while(temp>0) {
@@ -10,7 +13,7 @@ public class Pal {
             rev = rev*10+digit;
             temp /= 10;
         }
-        if(num==rev) System.out.println("Palindrome");
-        else System.out.println("Not Palindrome");
+        if(num==rev) System.out.println(num+" is a Palindrome");
+        else System.out.println(num+" is Not a Palindrome");
     }
 }
